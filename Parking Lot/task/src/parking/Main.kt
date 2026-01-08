028224d9cf09b1c1a648ab08ecac88d0226b261f
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-08 11:29:51 +0100

    stage 2

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,41 +1,38 @@
 package parking
 
-import java.awt.Color
-
-class Parking {
-    val spots: MutableList<Spot?> = MutableList(2) { null }
-
-    data class Spot(val plateNo: String, val color: String)
-
-    init {
-        spots[0] = Spot("KA-01-HH-1234", "Blue")
-    }
-
-    fun park(plateNo: String, color: String): String {
-        val index = spots.indexOfFirst { it == null }
-        spots[index] = Spot(plateNo, color)
-        return "$color car parked in spot ${index + 1}."
-
-    }
-
-    fun leave(spot: Int): String = if (isCarInSpot(spot))
-         "Spot $spot is free."
-    else
-         "There is no car in spot $spot."
-
-    private fun isCarInSpot(spot: Int) = spots[spot - 1] != null
-
-}
+data class Car(val regNumber: String, val color: String)
 
 fun main() {
-    val input = readln().substring(2).split(' ')
-    val parking = Parking()
-    val result = when (input.first()) {
-        "park" -> parking.park(input[1], input[2])
-        "leave" -> parking.leave(input[1].toInt())
-        else -> ""
-    }
-    println(result)
-}
+    val spots = arrayOfNulls<Car>(2)
+    spots[0] = Car("IT-DOESNT_MATTER", "Blue") // initial car
 
+    val input  = readln().trim().split(" ")
+
+    when (input[0]) {
+        "park" -> {
+            val regNumber = input[1]
+            val color = input[2]
+
+            if (spots[1] == null){
+                spots[1] = Car(regNumber, color)
+                println("$color car parked in spot 2.")
+            }
+        }
+        "leave" -> {
+            val spotNumber = input[1].toInt()
+
+            try {
+                val index = spotNumber - 1
+                if (spots[index] == null){
+                    println("There is no car in spot $spotNumber.")
+                } else {
+                    spots[index] = null
+                    println("Spot $spotNumber is free.")
+                }
+            } catch (e: IndexOutOfBoundsException) {
+                println("There is no car in spot $spotNumber.")
+            }
+        }
+    }
+}
 
